a8486854d43dfaa6510238b7a2bccee2f8c36942
Anzle <robleewilliams3@gmail.com>
2022-08-17 01:12:34 -0700

    reduce number of consts

--- 13 Roman to Integer.js
+++ 13 Roman to Integer.js
@@ -2,34 +2,32 @@
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
-    const ROMAN_NUMERALS = {"": 0, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
-    const EXCEPTION_NUMERALS = ["IV", "IX", "XL", "XC", "CD", "CM"];
-    
+ var romanToInt = function (s) {
+    const ROMAN_NUMERALS = { "": 0, "I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000 };
+
     // 1 <= s.length <= 15
     if (s.length < 1 || s.length > 15) return -1;
-    
+
     // create a running sum based on the math of the prior 2 characters seen
     let sum = 0;
     let priorS = "";
-    
+
     // s only contains the characters in ROMAN_NUMERALS
-    for (var i = 0; i < s.length ; i++) {
+    for (var i = 0; i < s.length; i++) {
         let nextS = s[i];
-        
         if (!nextS) return -1;
 
-        if (EXCEPTION_NUMERALS.includes(priorS+nextS)) {
-            sum += ROMAN_NUMERALS[nextS] - ROMAN_NUMERALS[priorS];
+        if (priorS && Object.hasOwn(ROMAN_NUMERALS, priorS + nextS)) {
+            sum += ROMAN_NUMERALS[priorS+nextS];
             priorS = "";
         }
         else {
-            sum = sum + ROMAN_NUMERALS[priorS];
+            sum += ROMAN_NUMERALS[priorS];
             priorS = nextS;
         }
     }
 
-    sum = sum + ROMAN_NUMERALS[priorS];
-    
+    sum += ROMAN_NUMERALS[priorS];
+
     return sum;
 };
