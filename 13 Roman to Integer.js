a9974483225ecf4101e3065ecccf1dfae1cee803
Anzle <robleewilliams3@gmail.com>
2022-08-17 00:56:12 -0700

    revisied solution

--- 13 Roman to Integer.js
+++ 13 Roman to Integer.js
@@ -2,25 +2,19 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+ var romanToInt = function(s) {
     const ROMAN_NUMERALS = {"": 0, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
     const EXCEPTION_NUMERALS = ["IV", "IX", "XL", "XC", "CD", "CM"];
     
     // 1 <= s.length <= 15
-    if (s.length < 1 || s.length > 15) return 0;
-    
-    // s only contains the characters in ROMAN_NUMERALS
-    for (var i = 0; i < s.length ; i++) {
-        if (!Object.hasOwn(ROMAN_NUMERALS, s[i]))
-            return 0;
-    }
+    // if (s.length < 1 || s.length > 15) return 0;
     
     // create a running sum based on the math of the prior 2 characters seen
     let sum = 0;
     let priorS = "";
     
-    
-    for (var i = 0; i < s.length; i++) {
+    // s only contains the characters in ROMAN_NUMERALS
+    for (var i = 0; i < s.length ; i++) {
         let nextS = s[i];
         
         if (EXCEPTION_NUMERALS.includes(priorS+nextS)) {
@@ -32,6 +26,7 @@ var romanToInt = function(s) {
             priorS = nextS;
         }
     }
+
     sum = sum + ROMAN_NUMERALS[priorS];
     
     return sum;
