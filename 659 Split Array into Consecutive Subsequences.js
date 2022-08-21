79b17620fd25a488b282e2297a78996680bf2d6c
Anzle <robleewilliams3@gmail.com>
2022-08-21 13:25:08 -0700

    no real changes here

--- 659 Split Array into Consecutive Subsequences.js
+++ 659 Split Array into Consecutive Subsequences.js
@@ -8,11 +8,13 @@
     
     let groups = [];
     let lowestSubI = 0;
+    let highestSubI = 0;
     
     // Scenario 1, increasing numbers, no repeates
     for(let i=0; i<nums.length;i++) {
         let placed = false;
         let gi = lowestSubI;
+        
         do {
             // bucket is empty
             if (!groups[gi]?.length) {
@@ -20,16 +22,16 @@
                 placed = true;
             }
             
+            // Same number, move to next bucket and try again
+            else if (nums[i] - groups[gi].at(-1) === 0) {
+                gi += 1;
+            }
+
             // 1 more than current bucket, add to subsqeuence
             else if (nums[i] - groups[gi].at(-1) === 1) {
                 groups[gi].push(nums[i]);
                 placed = true;
-            } 
-            
-            // Same number, move to next bucket and try again
-            else if (nums[i] - groups[gi].at(-1) === 0) {
-                gi += 1;
-            } 
+            }    
         
             // number too large for subsequence    
             else if (nums[i] - groups[gi].at(-1) > 1) {
@@ -37,6 +39,7 @@
                 if (groups[gi].length < 3) {
                     return false;
                 }
+                
                 // re-adjust our lowest staring subsequence and keep trying to place
                 else {
                     gi += 1;
