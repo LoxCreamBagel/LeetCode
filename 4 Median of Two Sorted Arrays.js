113b2c594bd6804e55facb6c96d99adc81309e77
Lox <LoxOnBagels@gmail.com>
2022-08-25 01:11:34 -0700

    Still working out kinks int he do/while trying to avoid looking up something

--- 4 Median of Two Sorted Arrays.js
+++ 4 Median of Two Sorted Arrays.js
@@ -22,16 +22,84 @@ var findMedianSortedArrays = function (nums1, nums2) {
   if (totalLength > 2000)
     throw Error("arrays too long > 2000");
 
-  let i = 0, j = 0, nums3 = [];
-  while(i+j < totalLength) {
-   nums3.push(
-    nums1[i] <= nums2[j] ? nums1[i++] : nums2[j++]
-   )
+
+  // Let's knock out easy usecases
+  let n = nums1, m = nums2;
+
+  // One of the lists is empty
+  if (n.length === 0) {
+    return listMedian(m);
+  } else if (m.length === 0) {
+    return listMedian(n);
   }
 
-  if (totalLength === 1) 
-    return nums3[0];
+  // all of 1 list is less than the other list
+  if (n[n.length-1] <= m[0]) {
+    return stackedListMedian(n, m)
+  } else if (m[m.length] <= n[0]) {
+    return stackedListMedian(m, n);
+  }    
+  
+  let i, j;
+  do {
+    // find the middle indexs
+    i = Math.floor(n.length/2);
+    j = Math.floor(m.length/2);
 
-  let k = Math.floor((totalLength-1)/2), l = Math.ceil((totalLength-1)/2);
-  return (nums3[k] + nums3[l]) / 2;
+    // The median of both lists are the same
+    // This is our median
+    if (n[i] === m[j]) {
+      return n[i];
+    }
+
+    // The median of n is smaller, search the upper portion
+    // The median of m is larger, search the lower portion, inclusive
+    else if (n[i] < m[j]) {
+      n = n.slice(i, n.length);
+      m = m.slice(0, j+1);
+    } 
+    
+    // The median of n is larger, search the lower portion, inclusive
+    // The median of m is larger, search the upper portion
+    else {
+      n = n.slice(0, i+1);
+      m = m.slice(j, m.length);
+    }
+  } while (n.length + m.length > 2);
+
 };
+
+// Only 1 list
+const listMedian = function(n) {
+  let k = Math.floor((n.length-1)/2), l = Math.ceil((n.length-1)/2);
+  return (n[k] + n[l]) / 2;
+}
+
+// all of 1 list is less than the other list
+const stackedListMedian = function (lower, upper) {
+  const totalLength = lower + upper;
+  const k = Math.floor((totalLength-1)/2), l = Math.ceil((totalLength-1)/2);
+
+  let lowerMedian = (k < lower.length) ? lower[k] : upper[k - lower.length];
+  let upperMedian = (l < lower.length) ? lower[l] : upper[l - lower.length];
+
+  return (lowerMedian + upperMedian) / 2;
+};
+
+var binarySearch = function (nums, target) {
+
+  let length = nums.length;
+  let middle = Math.floor(length / 2);
+
+  if (nums[middle] === target)
+    return middle;
+  
+  if (length === 1)
+    return -1;
+
+  if ( target < nums[middle])
+    return binarySearch(nums.slice(0, middle), target);
+
+  if (target > nums[middle])
+    return binarySearch(nums.slice(middle, length), target);
+}
